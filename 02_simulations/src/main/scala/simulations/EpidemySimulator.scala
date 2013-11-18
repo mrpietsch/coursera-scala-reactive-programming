325ebe30784a2b1a04b69a0f56cb11050c3952b3
Martin Pietsch <martin.pietsch@volkswagen.de>
2013-11-18 15:45:19 +0100

    guter Zwischenstand

--- 02_simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ 02_simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -11,14 +11,38 @@ class EpidemySimulator extends Simulator {
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
+    val prevalenceRate = 0.01
+    val transmissibilityRate = 0.4
+
+    val incubationTime = 6
+    val mortabilityTime = 14
+    val mortabilityRate = 0.25
+    val timeToGetImmune = 16
+    val timeToGetHealthy = 18
+
     // to complete: additional parameters of simulation
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = (for (i <- 0 to population - 1) yield new Person(i)).toList
 
-  class Person (val id: Int) {
+  val neighborsDeltas = List((1, 0), (0, 1), (-1, 0), (0, -1))
+
+  // initially infect certain persons
+  val initiallyInfected = (prevalenceRate * population).round.toInt
+  for (i <- 0 to initiallyInfected - 1) {
+    val randomIndex = randomBelow(300)
+    persons(randomIndex).infect()
+  }
+
+  // initially start moving
+  persons.foreach {
+    p => p.triggerMove()
+  }
+
+  class Person(val id: Int) {
+
     var infected = false
     var sick = false
     var immune = false
@@ -28,8 +52,94 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    def triggerMove() {
+      afterDelay(randomBelow(5) + 1)(move())
+    }
+
+    def beHealthy() = {
+      infected = false
+      // sick = false
+      immune = false
+    }
+
+    def beImmuneAndGetHealthySoon() = {
+      sick = false
+      immune = true
+      // infected = true
+      afterDelay(timeToGetHealthy - timeToGetImmune)(beHealthy())
+    }
+
+    def beDead() = {
+      dead = true
+      immune = false
+      sick = true
+      infected = true
+    }
+
+    def dieProbablyOrGetImmuneSoon() = {
+      if (random <= mortabilityRate) {
+        beDead()
+      } else {
+        afterDelay(timeToGetHealthy - mortabilityTime)(beImmuneAndGetHealthySoon())
+      }
+    }
+
+    def beSick() = {
+      sick = true
+      afterDelay(mortabilityTime)(dieProbablyOrGetImmuneSoon())
+    }
+
+    def infect() {
+      infected = true
+      afterDelay(incubationTime)(beSick())
+    }
+
+    def infectProbably() {
+      if (!infected && !immune) {
+        // find infected persons in the room
+        val infectedPersons = persons.filter(p => id != p.id && row == p.row && col == p.col && p.infected)
+        if (!infectedPersons.isEmpty) {
+          if (random <= transmissibilityRate) infect()
+        }
+      }
+    }
+
+    def calculateNeighbors() = {
+      neighborsDeltas.map {
+        case (rowDelta, colDelta) => ((row + rowDelta + roomRows) % roomRows, (col + colDelta + roomColumns) % roomColumns)
+      }
+    }
+
+    def personsInRoom(r: Int, c: Int) = {
+      persons.filter(p => id != p.id && r == p.row && c == p.col)
+    }
+
+    def move() {
+
+      val noOneIsSickPredicate: ((Int, Int)) => Boolean = {
+        case (r,c) => personsInRoom(r,c).filter(_.sick).isEmpty
+      }
+
+      // look around and filter rooms with obviously sick / dead persons
+      val walkableRooms: List[(Int, Int)] = for (
+        room <- calculateNeighbors() if noOneIsSickPredicate(room._1, room._2)
+      ) yield room
+
+      if ( ! walkableRooms.isEmpty ) {
+        val randomIndex: Int = randomBelow(walkableRooms.size)
+        val randomNeighbor: (Int, Int) = walkableRooms(randomIndex)
+
+        // go to that room
+        row = randomNeighbor._1
+        col = randomNeighbor._2
+
+        infectProbably()
+      }
+
+      if (!dead) triggerMove()
+    }
+
+
   }
+
 }
