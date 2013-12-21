c6a885bc2dfe2aafdb3d2e0dd33b4047435303a5
Martin Pietsch <martin@p-space.org>
2013-12-21 01:20:17 +0100

    Step3 working

--- 06_kvstore/src/main/scala/kvstore/Replicator.scala
+++ 06_kvstore/src/main/scala/kvstore/Replicator.scala
@@ -1,9 +1,9 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
+import akka.actor._
 import scala.concurrent.duration._
+import scala.Some
+import scala.language.postfixOps
 
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
@@ -15,14 +15,26 @@ object Replicator {
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
 }
 
-class Replicator(val replica: ActorRef) extends Actor {
+class Replicator(val replica: ActorRef) extends Actor with ActorLogging{
   import Replicator._
   import Replica._
   import context.dispatcher
-  
+
+
+  // we want to receive Terminated messages but we won't process
+  // them... hence we die with the replica...
+  override val supervisorStrategy = OneForOneStrategy(maxNrOfRetries = 5, withinTimeRange = 100 milliseconds, loggingEnabled = true) {
+    case _ => SupervisorStrategy.escalate
+  }
+  context.watch(replica)
+
+
+  context.system.scheduler.schedule(0 seconds, 100 milliseconds, self, "processBatch")
+
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
+
 
   // map from sequence number to pair of sender and request
   var acks = Map.empty[Long, (ActorRef, Replicate)]
@@ -37,20 +49,30 @@ class Replicator(val replica: ActorRef) extends Actor {
   }
 
   def receive: Receive = {
-    case r@Replicate(key: String, valueOption: Option[String], id: Long) =>
+
+    case "processBatch" =>
+      pending.foreach( replica ! _)
+
+    case r@Replicate(key, valueOption, id) =>
       val seq = nextSeq
       acks = acks + (seq ->(sender, r))
-      replica ! Snapshot(key, valueOption, seq)
+      val snapshot: Snapshot = Snapshot(key, valueOption, seq)
+      pending = pending :+ snapshot
 
-    case SnapshotAck(key: String, seq: Long) =>
+    case SnapshotAck(key, seq) =>
       acks.get(seq) match {
         case Some((senderRef, originalReplicateRequest)) =>
-          acks = acks - seq
-          senderRef ! Replicated(originalReplicateRequest.key, originalReplicateRequest.id)
-        case None => // todo
+          if ( pending.head.seq == seq) {
+            log.info("rcv " + seq)
+            pending = pending.tail
+            acks = acks - seq
+            senderRef ! Replicated(originalReplicateRequest.key, originalReplicateRequest.id)
+          }
+
+        case None => // todo oder oben mit ask pattern arbeiten
       }
 
-
+    case ReceiveTimeout => log.info("TIMEOUT")
   }
 
 }
